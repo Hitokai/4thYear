31d44d0058c7b0d91b0cf7d42c0fa0dd7f92e20e
Artem Simbirtsev <temik19199@gmail.com>
2018-11-05 23:31:24 +0300

    add sqlDB

---
+++ Programs/sqlDB/sqlDB/DBConnect.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace sqlDB
+{
+    class DBConnect
+    {
+        static string connStr = "server=rooddie.ddns.net;user=root;database=cdb;password=basavka303;";
+
+        public static void RegFunc(string newLogin, string newPassword, string newEmail, string newFname, string newLname)
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+
+            string query = String.Format("INSERT INTO users(login, password, email, fname, lname) VALUES('{0}', SHA('{1}'), '{2}', '{3}', '{4}')",
+                newLogin, newPassword, newEmail, newFname, newLname);
+
+            MySqlCommand command = new MySqlCommand(query, conn);
+            // выполняем запрос
+            command.ExecuteNonQuery();
+            // закрываем подключение к БД
+            conn.Close();
+        }
+
+        public static void LoginFunc(string newLogin, string newPassword)
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+
+            // запрос
+            string query = String.Format("SELECT * FROM users WHERE '{0}' in (SELECT login FROM users) AND SHA('{1}') in (SELECT password FROM users)",
+                newLogin, newPassword);
+            // объект для выполнения SQL-запроса
+            MySqlCommand command = new MySqlCommand(query, conn);
+            // объект для чтения ответа сервера
+            MySqlDataReader reader = command.ExecuteReader();
+            // читаем результат
+            if (reader.Read())
+                MessageBox.Show("ВЫ ВОШЛИ");
+            else
+                MessageBox.Show("ВЫЙДИ В ОКНО");
+
+            reader.Close(); // закрываем reader
+            // закрываем соединение с БД
+            conn.Close();
+        }
+    }
+}
---
+++ Programs/sqlDB/sqlDB/reg_page.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sqlDB
+{
+    /// <summary>
+    /// Логика взаимодействия для reg_page.xaml
+    /// </summary>
+    public partial class reg_page : UserControl
+    {
+        public reg_page()
+        {
+            InitializeComponent();
+        }
+
+        private void backToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            UserControl usc = null;
+            pageContent.Children.Clear();
+            pageContent.RowDefinitions.Clear();
+
+            usc = new login_template();
+            pageContent.Children.Add(usc);
+        }
+
+        private void regBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginBlock.Text;
+            string password = passwordBlock.Password;
+            string email = emailBlock.Text;
+            string fname = fnameBlock.Text;
+            string lname = lnameBlock.Text;
+
+            sqlDB.DBConnect.RegFunc(login, password, email, fname, lname);
+
+            loginBlock.Clear();
+            passwordBlock.Clear();
+            emailBlock.Clear();
+            fnameBlock.Clear();
+            lnameBlock.Clear();
+        }
+    }
+}
